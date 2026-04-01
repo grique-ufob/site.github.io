babd5d68b13cd6dd0e06f65b7f7db5166f6e68b7
Czar_Razec <cezarcarmofilho@gmail.com>
2026-04-01 17:41:00 -0300

    Organização do projeto, separando funcionalidades em arquivos diferentes/ script.js & style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+ function changeLang(lang) {
+        const ptElements = document.querySelectorAll('.lang-pt');
+        const enElements = document.querySelectorAll('.lang-en');
+        const btnPt = document.getElementById('btn-pt');
+        const btnEn = document.getElementById('btn-en');
+
+        if (lang === 'en') {
+            ptElements.forEach(el => el.style.display = 'none');
+            enElements.forEach(el => el.style.display = 'block');
+            btnEn.classList.add('active');
+            btnPt.classList.remove('active');
+        } else {
+            ptElements.forEach(el => el.style.display = 'block');
+            enElements.forEach(el => el.style.display = 'none');
+            btnPt.classList.add('active');
+            btnEn.classList.remove('active');
+        }
+    }
